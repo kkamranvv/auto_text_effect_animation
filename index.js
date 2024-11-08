30a0617553067aa5f9623fac604b014353566ee0
kkamranvv <musadirlikamran@gmail.com>
2024-11-08 22:26:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const containerEl = document.querySelector(".container");
+
+const careers = ["Frontend Developer", "Web Developer", "Freelancer", "Cook"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+<h1>I am a ${careers[careerIndex].slice(0, characterIndex)}</h1>
+`;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+
+  setTimeout(updateText, 300);
+}
+
+updateText();
